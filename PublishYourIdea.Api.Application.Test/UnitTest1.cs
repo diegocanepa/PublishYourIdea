032ed2ed3f7db10cf5901cbe4a4c55ef07a795b4
diegocanepa <diego_canepa98@hotmail.com>
2020-12-29 16:43:46 -0300

    General Structure of the Proyect

---
+++ PublishYourIdea.Api.Application.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PublishYourIdea.Api.Application.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
